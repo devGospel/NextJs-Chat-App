9d116cb40598643a54d43aff5d6fedcaedfa4f5d
devGospel <ozchange2002@gmail.com>
2024-09-17 11:48:41 +0100

    Reapply "Update page.jsx"
    This reverts commit 247910129d0544461632adff7831110dbadec877.

--- app/page.jsx
+++ app/page.jsx
@@ -5,16 +5,15 @@ import Feed from '../components/Feed'
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
-        <h1 className='head_text text-center'> Discover & Share
+        <h1 className='head_text text-center'> Connect & Chat
             <br className='max-md:hidden' />
             <span className='orange_gradient
-            text-center'> AI-Powered Prompts
+            text-center'> Connect to the world
             </span>
         </h1>
         <p className='desc text-center'>
-            Promptopia is an open-source AI Prompting 
-            tool for modern world to
-            discover, create and share creative prompts
+            This web chatter connects you to the world
+            of your choice, family, loved ones & friends
         </p>
 
         <Feed/>
